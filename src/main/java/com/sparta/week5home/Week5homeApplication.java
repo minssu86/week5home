02704a51090b563705237afe987b7829700a00a6
minssu86 <manager.kim86@gmail.com>
2022-06-03 10:19:36 +0900

    6/23
    create project

---
+++ src/main/java/com/sparta/week5home/Week5homeApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.week5home;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week5homeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Week5homeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/week5home/Week5homeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.week5home;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week5homeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
